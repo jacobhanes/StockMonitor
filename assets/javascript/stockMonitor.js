1eaec7a2e094f262bbe55216b7e31dba7ab10858
Sam Estorga <estorgajr@gmail.com>
2019-07-02 19:13:46 -0700

    added Firebase stuff

--- assets/javascript/stockMonitor.js
+++ assets/javascript/stockMonitor.js
@@ -1,3 +1,40 @@
+// Initialize Firebase
+const Config = {
+    apiKey: "AIzaSyCsZ3wNbJL4YxJs49j6tBUYcPmtwTFZFNQ",
+    authDomain: "stockmonitor-37fc2.firebaseapp.com",
+    databaseURL: "https://stockmonitor-37fc2.firebaseio.com",
+    projectId: "stockmonitor-37fc2",
+    storageBucket: "",
+    messagingSenderId: "158264431131",
+    appId: "1:158264431131:web:44a1b828a85e9390"
+  };
+firebase.initializeApp(Config);
+
+let stockObj = {};
+stockObj.stockName = "Apple";
+stockObj.quantity = 10;
+stockObj.price = 10.56;
+stockObj.purchaseDate = "mm/dd/yyyy";
+stockObj.user = 1;
+
+let userObj = {};
+userObj.userName = "Sam";
+userObj.email = "name@gmail.com";
+userObj.user = 1;
+
+let stockRef = firebase.database();
+
+stockRef.ref('/stocks').on('child_added',function(childObj, prevChildKeyObj){
+    console.log(childObj.key);
+    console.log(childObj.val());
+
+});
+console.log(stockObj);
+stockRef.ref('/stocks').push(stockObj);
+
+
+ 
+
 $(document).ready(function(eventObj){
     console.log(eventObj);
 
@@ -18,6 +55,8 @@ function getStockData(stockElement){
         console.log(resObj["Meta Data"]["2. Symbol"]);
         console.log(resObj["Meta Data"]["3. Last Refreshed"]);
         console.log(resObj["Time Series (Daily)"]["2019-06-28"]["4. close"]);
+
+        $("#stockTable > tbody").append($("<tr>"),$("<td>").text(resObj["Meta Data"]["2. Symbol"]));
     
     
     });
@@ -27,6 +66,8 @@ function getStockData(stockElement){
 for(let i=0;i<stockNames.length;i++){
     getStockData(stockNames[i]);
 }
+
+
 
 
 });
