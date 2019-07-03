ae3c48fcb560211d3f8672171dd607e27703e3a2
Jogeswararao Kolla <jogeskolla@gmail.com>
2019-07-02 19:39:55 -0700

    updated the js

--- assets/javascript/stockMonitor.js
+++ assets/javascript/stockMonitor.js
@@ -28,37 +28,10 @@ let stockRef = firebase.database();
 stockRef.ref('/stocks').on('child_added',function(childObj, prevChildKeyObj){
     console.log(childObj.key);
     console.log(childObj.val());
-
 });
 console.log(stockObj);
 stockRef.ref('/stocks').push(stockObj);
 
-
- 
-
-$(document).ready(function(eventObj){
-    console.log(eventObj);
-
-let baseUrl="https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&interval=5min&outputsize=full";
-let stockSymbolName="&symbol="
-let stockNames=["MSFT","TSLA","AXP"];
-let stockApiKey="&apikey=9C5L7VDS9B25DUYZ";
-    
-function getStockData(stockElement){
-
-    $.ajax({
-        url : baseUrl + stockSymbolName + stockElement + stockApiKey,
-        method: "GET"
-    }).then(function(resObj){
-        console.log(baseUrl + stockSymbolName + stockElement + stockApiKey);
-        console.log(resObj);
-        console.log(resObj["Meta Data"]);
-        console.log(resObj["Meta Data"]["2. Symbol"]);
-        console.log(resObj["Meta Data"]["3. Last Refreshed"]);
-        console.log(resObj["Time Series (Daily)"]["2019-06-28"]["4. close"]);
-
-        $("#stockTable > tbody").append($("<tr>"),$("<td>").text(resObj["Meta Data"]["2. Symbol"]));
-    
 
 $(document).ready(function (eventObj) {
 
